c9cc0f71567847297a9746d3dc44a783f59aa887
Dalton Stegner <dmstegner@gmail.com>
2025-07-24 20:48:55 -0400

    tl-10

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hellow World
+    </h1>
+    <button class="btn btn-secondary">
+      View Map <Icon name="tabler:map" size="32" />
+    </button>
   </div>
 </template>
